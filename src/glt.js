3650c0b66507d3b7097a9ac8cb502a12a37e7ba1
Nick Shine <shine.nick@gmail.com>
2019-03-17 21:59:04 -0500

    handle subcommand validation

--- src/glt.js
+++ src/glt.js
@@ -2,7 +2,8 @@
 
 const program = require('commander');
 const { version } = require('../package.json');
-const logger = require('./logger');
+const logger = require('./lib/logger');
+const { validateCommand } = require('./lib/validate');
 
 program
   .version(version, '-V, --version')
@@ -14,34 +15,6 @@ program
   .command('ci', 'perform GitLab CI tasks')
   .on('option:verbose', () => {
     logger.transports.find(t => t.name === 'console').level = 'debug';
-    logger.debug('Verbose output enabled.');
   })
-  .on('command:*', function (command) {
-    const cmd = command[0];
-    // eslint-disable-next-line no-underscore-dangle,eqeqeq
-    if (!this.commands.find(c => c._name == cmd)) {
-      logger.error(`Invalid command: ${this.args.join(' ')}\n-------------------`);
-      program.help();
-    }
-  })
+  .on('command:*', validateCommand)
   .parse(process.argv);
-
-if (!program.token) {
-  logger.debug("'--token' flag not provided, falling back to GITLAB_TOKEN env var.");
-  program.token = process.env.GITLAB_TOKEN;
-  if (!program.token) {
-    logger.debug('GITLAB_TOKEN env var not set, falling back to CI_JOB_TOKEN env var.');
-    program.token = process.env.CI_JOB_TOKEN;
-  }
-  if (!program.token) {
-    logger.error('CI_JOB_TOKEN env var not set. A token is required.');
-    process.exit(1);
-  }
-} else if (!program.projectId) {
-  logger.debug("'--project-id' flag not provided, falling back to CI_PROJECT_ID env var.");
-  program.projectId = process.env.CI_PROJECT_ID;
-  if (!program.projectId) {
-    logger.debug('CI_PROJECT_ID env var not set. A project-id is required.');
-    process.exit(1);
-  }
-}
